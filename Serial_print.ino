071342f43c808a6255cb8b8a8ac86d008be51662
Niwantha <niwantha33@gmail.com>
2018-01-25 22:24:40 +0530

    Create Serial_print.ino

---
+++ Serial_print.ino
@@ -0,0 +1,14 @@
+/*
+ * Author : Niwantha 
+ * src: Arduino Refrences & Espressif  Arduino core 
+ * example: EasyNode Serial print 
+ * 
+ */
+
+void setup() {
+ Serial.begin(115200);
+}
+
+void loop() {
+  Serial.print("Hi EasyNode- Go \n");
+}
